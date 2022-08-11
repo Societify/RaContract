5ceaca267536dc1ea0b6ae5c88cb45a32f94d15c
RacoonLife <94892674+RacoonLife@users.noreply.github.com>
2022-08-11 09:16:16 +0800

    Update IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -26,6 +26,5 @@ interface IERC20 {
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
